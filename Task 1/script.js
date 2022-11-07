4fa75e225dfaa42a638130981f6bdaa33c5914a0
VytautasSts <111770550+VytautasSts@users.noreply.github.com>
2022-11-07 19:52:43 +0200

    Completed task 1
    Task 1 files updated

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,32 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+document.getElementById("submit-btn").addEventListener("click", convertWeight);
+
+function convertWeight() {
+    let kg = parseInt(document.getElementById('search').value);
+    let lb = getPounds(kg);
+    let g = getGrams(kg);
+    let oz = getOunces(kg);
+    showResult(kg, lb, g, oz);
+}
+
+function getPounds(weight) {
+    let pounds = weight * 2.2046;
+    return pounds;
+}
+function getGrams(weight) {
+    let grams = weight / 0.001;
+    return grams;
+}
+function getOunces(weight) {
+    let ounces = weight * 35.274;
+    return ounces;
+}
+
+function showResult(kg, lb, g, oz) {
+    document.getElementById("output").style.visibility = "visible";
+    document.getElementById('pounds').innerHTML = kg+" kg is equal to "+lb+" pounds."
+    document.getElementById('grams').innerHTML = kg+" kg is equal to "+g+" grams."
+    document.getElementById('ounces').innerHTML = kg+" kg is equal to "+oz+" pounds."
+}
