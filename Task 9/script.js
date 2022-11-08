38122a57924c7ed80dd19e4b5a7974ce22e8a1ef
VytautasSts <111770550+VytautasSts@users.noreply.github.com>
2022-11-08 18:56:06 +0200

    Completed task 9
    Task 9 files updated

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+let title = "";
+let director = "";
+let budget = 0;
+
+let movie = new Movie();
+
+function Movie() {
+    this.read = function () {
+        this.title = prompt("Įveskite filmo pavadinimą:", "");
+        this.director = prompt("Įveskite filmo ražisierių:","");
+        this.budget = parseInt(prompt("Įveskite filmo biudžetą:", 0));
+    }
+    this.wasExpensive = function () {
+        return this.budget>100000000?true:false;
+    
+}
+}
+
+movie.read();
+console.log("Movie tile: "+movie.title);
+console.log("Movie director: " +movie.director);
+console.log("Ar brangus filmas? "+ movie.wasExpensive());
